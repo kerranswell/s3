30a12491c9f204adf674bfd9161dd60a8974a34c
Konstantin Kerranswell <konstantin@MacBook-Pro-Konstantin.local>
2015-11-05 03:40:14 +0400

    new pics added

--- core/db_access.php
+++ core/db_access.php
@@ -1,6 +1,6 @@
 <?php
     define("DB_HOST", "localhost");
-    define("DB_BASE", "s3");
-    define("DB_USER", "root");
-    define("DB_PASS", "");
+    define("DB_BASE", "srv64845_s3");
+    define("DB_USER", "srv64845_s3");
+    define("DB_PASS", "dRs33sg5F");
 
