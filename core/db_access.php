371ad7510bbeee7f8a51e28f358d266feb404b7f
Konstantin Tsikolenko <ktsikolenko@hsmedia.ru>
2015-11-05 18:59:02 +0300

    inp

--- core/db_access.php
+++ core/db_access.php
@@ -1,6 +1,11 @@
 <?php
-    define("DB_HOST", "localhost");
+/*    define("DB_HOST", "localhost");
     define("DB_BASE", "srv64845_s3");
     define("DB_USER", "srv64845_s3");
     define("DB_PASS", "dRs33sg5F");
+*/
+    define("DB_HOST", "localhost");
+    define("DB_BASE", "s3");
+    define("DB_USER", "root");
+    define("DB_PASS", "");
 
